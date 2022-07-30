63564c2386349b1dbdd96474d972599fc0cd1418
Matheus Amaral <jsmatheusoliveira7@gmail.com>
2022-07-30 19:41:15 -0300

    add variavel de ambiente ao projeto

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
-const PORT = 3334
-// app.use(express.json())
+const PORT = process.env.PORT || 3333
+
+app.use(express.json())
 
 app.listen(PORT,  () => {
     console.log('Running in port',PORT)
